b63d28b2827d102f33afd59240dcbdebb2b72eeb
VocanicZ <ampnattahapat@gmail.com>
2024-10-08 01:49:34 +0700

    test1.1

--- GameSwitcher/GameSwitcher.cs
+++ GameSwitcher/GameSwitcher.cs
@@ -14,7 +14,12 @@ internal sealed class GameSwitcher : IGitHubPluginUpdates {
 	public Version Version => typeof(GameSwitcher).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 
 	public Task OnLoaded() {
-		ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
+		string[] header =  {"#####################################################################################",							"#  _____       _       ____                 _____                _   _              #",							"# |  __ \\     (_)     |  _ \\               / ____|              | | (_)             #",							"# | |__) |__ _ _ _ __ | |_) | _____      _| |     _ __ ___  __ _| |_ _  ___  _ __   #",							"# |  _  // _` | | '_ \\|  _ < / _ \\ \\ /\\ / / |    | '__/ _ \\/ _` | __| |/ _ \\| '_ \\  #",							"# | | \\ \\ (_| | | | | | |_) | (_) \\ V  V /| |____| | |  __/ (_| | |_| | (_) | | | | #",							"# |_|  \\_\\__,_|_|_| |_|____/ \\___/ \\_/\\_/  \\_____|_|  \\___|\\__,_|\\__|_|\\___/|_| |_| #",
+							"#                                                                                   #",
+							"##############################################################GameSwitcher 1.0.0#####"};
+		foreach (string line in header) {
+			ASF.ArchiLogger.LogGenericInfo($"{line}");
+		}
 
 		return Task.CompletedTask;
 	}
